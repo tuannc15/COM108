572396d67c7cef3cf45a4a516199aa5cbd407a0d
nctuan2013 <54241848+nctuan2013@users.noreply.github.com>
2023-10-04 13:06:06 +0700

    Tạo Bài 1

--- Bài 1/Bai1.c
+++ Bài 1/Bai1.c
@@ -4,8 +4,6 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	printf("Hello World.");
-	system("pause");
-	getch();
+	printf("Bai so 1:");
 	return 0;
 }
